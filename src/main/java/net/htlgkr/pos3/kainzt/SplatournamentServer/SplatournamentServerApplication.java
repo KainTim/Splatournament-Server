c1b512b2b7f43a51e3c5952909a7f40923b11c01
Tim Kainz <kainzt21030@sus.htl-grieskirchen.at>
2024-04-18 08:03:40 +0200

    Created Project

---
+++ src/main/java/net/htlgkr/pos3/kainzt/SplatournamentServer/SplatournamentServerApplication.java
@@ -0,0 +1,13 @@
+package net.htlgkr.pos3.kainzt.SplatournamentServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplatournamentServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplatournamentServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/htlgkr/pos3/kainzt/SplatournamentServer/SplatournamentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.htlgkr.pos3.kainzt.SplatournamentServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplatournamentServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
