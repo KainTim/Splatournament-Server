2a7fd900b5e8762c16d1795d9edf84576d1ed924
Tim Kainz <tikaiz@gmx.at>
2024-04-19 20:02:46 +0200

    changed Path of Tournament Controller to Tournaments

--- src/main/java/net/htlgkr/pos3/kainzt/SplatournamentServer/controllers/TournamentController.java
+++ src/main/java/net/htlgkr/pos3/kainzt/SplatournamentServer/controllers/TournamentController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/users")
+@RequestMapping("/api/tournaments")
 public class TournamentController {
 
 }
