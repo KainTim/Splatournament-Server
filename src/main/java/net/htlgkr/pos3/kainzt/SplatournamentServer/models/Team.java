75a9606510cdf66cc06508ddb47388c8255f0d79
Tim Kainz <kainzt21030@sus.htl-grieskirchen.at>
2024-04-19 12:08:59 +0200

    changed annotations to match template

--- src/main/java/net/htlgkr/pos3/kainzt/SplatournamentServer/models/Team.java
+++ src/main/java/net/htlgkr/pos3/kainzt/SplatournamentServer/models/Team.java
@@ -2,6 +2,8 @@ package net.htlgkr.pos3.kainzt.SplatournamentServer.models;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -9,7 +11,8 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
-@Table(name = "team")
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
